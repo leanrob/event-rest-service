c5f1d78a6382539e195af782930d14d60a2b621e
Robert Byrne <mr.rob.byrne@gmail.com>
2016-08-28 01:21:02 -0230

    Setup simple routing for the webapp
    - setup a router using ‘express.router()
    - told the ‘/events’ route to use the eventRouter()
    - setup a route for ‘/books’ and ‘/books/single’
    - all is working well

--- events/app.js
+++ events/app.js
@@ -4,12 +4,36 @@ var app = express();
 
 var port = Number(process.env.PORT) || 1337;
 
+var eventRouter = express.Router();
+
 // Node will check the public and src views directories before all else...
 app.use(express.static('public'));
 app.set('views', 'src/views');
 
 app.set('view engine', 'ejs');
 
+/*
+ Books Router Section Begins
+ */
+
+eventRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Events');
+    });
+
+eventRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Hello Single Event');
+    });
+
+// Telling '/Books' that it uses eventRouter
+app.use('/events', eventRouter);
+
+/*
+Books Router Section Ends
+*/
+
+// Navigation setup
 app.get('/', function(req, res) {
     res.render('index', {
         title: 'Hello from render',
@@ -21,10 +45,6 @@ app.get('/', function(req, res) {
             Text: 'Authors'
         }]
     });
-});
-
-app.get('/books', function(req, res) {
-    res.send('Hello Books');
 });
 
 app.listen(port, function(err) {
