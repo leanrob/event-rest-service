cbfaf5ee73187aede983fafba35d191fb1af7e29
Robert Byrne <mr.rob.byrne@gmail.com>
2016-08-31 17:04:29 -0230

    NPM install mongodb and test it
    NPM install mongodb and test it
    - npm install mongodb —save
    - create a connection in adminRoutes.js and test to make sure the program can build.
    - Adding some events to the mongodb
    - test inserting by going to localhost/Admin/addEvents

--- events/src/routes/adminRoutes.js
+++ events/src/routes/adminRoutes.js
@@ -4,11 +4,55 @@
 var express = require('express');
 var adminRouter = express.Router();
 
+var mongodb = require('mongodb').MongoClient;
+
+var eventGroup = [
+    {
+        course: 'CS101',
+        title: 'Assignment 1',
+        day: 'monday',
+        checked: false
+    },
+    {
+        course: 'CS101',
+        title: 'Assignment 2',
+        day: 'wednesday',
+        checked: false
+    },
+    {
+        course: 'CS101',
+        title: 'Assignment 3',
+        day: 'sunday',
+        checked: false
+    },
+    {
+        course: 'CS101',
+        title: 'Midterm',
+        day: 'fursday',
+        checked: true
+    },
+    {
+        course: 'CS101',
+        title: 'Final',
+        day: 'tuesday',
+        checked: false
+    }
+];
+
 var router = function (nav) {
 
     adminRouter.route('/addEvents')
         .get(function (req, res) {
-            res.send('inserting books');
+            var url = 'mongodb://localhost:27017/eventsApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('events');
+                collection.insertMany(eventGroup, function (err, results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
+            // res.send('inserting books');
         });
 
     return adminRouter;
