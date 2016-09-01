51b0b8687d83ebe7aef116ce799f6a94de5f7312
Robert Byrne <mr.rob.byrne@gmail.com>
2016-08-31 22:20:48 -0230

    Change the single event route to use MongoDB
    - removed the boks array
    - added it to MongoDB
    - setup the rutes for the single books by routing them using their ObjectId in the MongoDB database.
    - tested and working correctly
      - after a hell of a time debugging.

--- events/src/routes/eventGroupRoutes.js
+++ events/src/routes/eventGroupRoutes.js
@@ -5,38 +5,9 @@ var express = require('express');
 
 var eventGroupRouter = express.Router();
 
-var eventGroup = [
-    {
-        course: 'CS101',
-        title: 'Assignment 1',
-        day: 'monday',
-        checked: false
-    },
-    {
-        course: 'CS101',
-        title: 'Assignment 2',
-        day: 'wednesday',
-        checked: false
-    },
-    {
-        course: 'CS101',
-        title: 'Assignment 3',
-        day: 'sunday',
-        checked: false
-    },
-    {
-        course: 'CS101',
-        title: 'Midterm',
-        day: 'fursday',
-        checked: true
-    },
-    {
-        course: 'CS101',
-        title: 'Final',
-        day: 'tuesday',
-        checked: false
-    }
-];
+var mongodb = require('mongodb').MongoClient;
+
+var objectId = require('mongodb').ObjectID;
 
 /*
  Events Router Section Begins
@@ -44,32 +15,46 @@ var eventGroup = [
 
 eventGroupRouter.route('/')
     .get(function(req, res) {
-        res.render('eventGroupView', {
-            title: 'eventGroup',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            eventGroup: eventGroup
+        var url = 'mongodb://localhost:27017/eventsApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('events');
+            collection.find({}).toArray(function (err, results) {
+                res.render('eventGroupView', {
+                    title: 'eventGroup',
+                    nav: [{
+                        Link: '/Books',
+                        Text: 'Books'
+                    }, {
+                        Link: '/Authors',
+                        Text: 'Authors'
+                    }],
+                    eventGroup: results
+                });
+            })
         });
     });
 
 eventGroupRouter.route('/:id')
     .get(function(req, res) {
-        var id = req.params.id;
-        res.render('eventSingleView', {
-            title: 'eventSingle',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            }, {
-                Link: '/Authors',
-                Text: 'Authors'
-            }],
-            eventSingle: eventGroup[id]
+        var id = new objectId(req.params.id);
+        var url = 'mongodb://localhost:27017/eventsApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('events');
+            collection.findOne({_id: id}, function (err, results) {
+                res.render('eventSingleView', {
+                    title: 'eventSingle',
+                    nav: [{
+                        Link: '/Books',
+                        Text: 'Books'
+                    }, {
+                        Link: '/Authors',
+                        Text: 'Authors'
+                    }],
+                    eventSingle: results
+                });
+            });
         });
     });
 
