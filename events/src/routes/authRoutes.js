96251205fe6180b0c484fa391ab579407ce5bf03
Robert Byrne <mr.rob.byrne@gmail.com>
2016-09-05 18:55:35 -0230

    Fixes #9 Install and test mongoose
    This commit is ONLY to install mongoose and test that it is in the package.json file

--- events/src/routes/authRoutes.js
+++ events/src/routes/authRoutes.js
@@ -7,23 +7,33 @@ var authRouter = express.Router();
 
 var mongodb = require('mongodb').MongoClient;
 
+var passport = require('passport');
+
 var router = function() {
+    // routhe for user to sign-up
     authRouter.route('/signUp')
         .post(function(req, res) {
             console.log(req.body);
             var url = 'mongodb://localhost:27017/eventsApp';
             mongodb.connect(url, function(err, db) {
                 var collection = db.collection('users');
+                // TODO: Change with full user when it is created
                 var user = {
                     username: req.body.userName,
                     password: req.body.password
                 };
-                collection.insert(user, function (err, results) {
+                collection.insertOne(user, function (err, results) {
                     req.login(results.ops[0], function() {
                         res.redirect('/auth/profile');
                     });
                 });
             });
+        });
+    authRouter.route('/signIn')
+        .post(passport.authenticate('local', {
+            failureRedirect: '/'
+        }), function(req, res) {
+            res.redirect('auth/profile');
         });
     authRouter.route('/profile')
         .get(function(req, res) {
